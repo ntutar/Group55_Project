4a6c4127363b5d1d9ab090d0cf9bc59ec6d3b29d
ntutar <nazmitutar@gmail.com>
2021-02-25 10:25:18 +1000

    created new project and classes

--- src/main/java/WonderWoman.java
+++ src/main/java/WonderWoman.java
@@ -1,2 +1,8 @@
 public class WonderWoman {
+    public static void main(String[] args) {
+        System.out.println("Merhaba");
+        System.out.println("Merhaba");
+        System.out.println("Merhaba");
+        System.out.println("Merhaba");
+    }
 }
