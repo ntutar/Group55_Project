770944ea2b4ebae968afdf13319731b81a0de30a
ntutar <nazmitutar@gmail.com>
2021-02-25 12:08:57 +1000

    added forFeatureA.java class

--- src/main/java/forUs_123.java
+++ src/main/java/forUs_123.java
@@ -5,5 +5,7 @@ public class forUs_123 {
         System.out.println("3");
         System.out.println("4");
         System.out.println("5");
+        System.out.println("6");
+        System.out.println("7");
     }
 }
