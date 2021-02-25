16e76de7688adddd81dceb42a353c0baaa09c24a
ntutar <nazmitutar@gmail.com>
2021-02-25 20:05:16 +1000

    added a line more on WonderWoman.java

--- src/main/java/WonderWoman.java
+++ src/main/java/WonderWoman.java
@@ -8,6 +8,7 @@ public class WonderWoman {
         System.out.println("2");
         System.out.println("3");
         System.out.println("voila");
+        System.out.println("senasamisezai");
 
     }
 }
